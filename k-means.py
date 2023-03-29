c2a00c7b592859a05e099d07eec20c4d786ed11c
Davide Belcastro <117814083+davidebelcastro-sig@users.noreply.github.com>
2023-03-29 18:08:48 +0200

    Add files via upload

--- k-means.py
+++ k-means.py
@@ -13,6 +13,7 @@ def loadImage(path):
 
 def filter(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
     #applica piu filtri possibili
     return img
 
@@ -73,17 +74,36 @@ def showElbowMethod(ks,inertias):
 
 #trovare se c'è il tumore
 #0 se non c'è, 1 se c'è
-def findTumor(img):
-    pass
-    return -1
+def findTumor(original,img):
+    #binarizzo
+    ret, thresh = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+    #trovo i contorni
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # escludi i contorni più piccoli della soglia specificata
+    filtered_contours = []
+    min_area = 300
+    tumor = -1
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area >= min_area and area <= 10000:  #capire se è il valore giusto
+            filtered_contours.append(contour)
+            tumor = 1
+    cv2.drawContours(original, filtered_contours, -1, (0, 255, 0), 2)
+    # mostra l'immagine con i contorni filtrati
+    cv2.imshow('Contorni', original)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    if tumor == 1:
+        return 1, filtered_contours
+    else:
+        return 0, None
 
-
-def studyContour(img, segmented_img):
+def studyContour(original, lista_contorni):
     pass
 
 
 def main():
-    path = ".image.jpg"
+    path = "no.jpg"
     iterations = 15  #numero di k da testare
     img = loadImage(path)
     img_copia = img.copy()
@@ -91,14 +111,18 @@ def main():
     data = convertInNpArray(img)
     k, inertias, ks = findK(data, iterations)
     segmented_img = KMeansClustering(data, k, img)
+    print("k trovato= "+str(k))
     showImage(segmented_img)
     showElbowMethod(ks, inertias)
     #vedere se l'immagine segmentata ha un tumore o no
     #se ce l'ha allora studiare il contorno
     #generare file output con tutte le caratteristiche del tumore
-    tumor = findTumor(segmented_img)
+    img_to_draw = img_copia.copy()
+    result = findTumor(img_to_draw,segmented_img)
+    tumor = result[0]
     if tumor == 1:
-        studyContour(img_copia, segmented_img)
+        contours = result[1]
+        studyContour(img_copia, contours)
     else:
         print("Non ci sono tumori")
 
