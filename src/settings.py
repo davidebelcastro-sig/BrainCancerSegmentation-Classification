00e07fab623b089e1fe54e13293c110fa6a646ca
Davide Belcastro <davide_belcastro@yahoo.it>
2023-10-22 15:46:02 +0200

    upload

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,4 @@
 """Insert here the path to the tmp folder"""
-path_to_tmp = "/Users/lucian/GitHub/BrainCancerSegmentation-Classification/tmp"
-path_to_model = "/Users/lucian/GitHub/BrainCancerSegmentation-Classification/src/tumorclassification/modello_rf.pkl"
-path_to_filters = "/Users/lucian/GitHub/BrainCancerSegmentation-Classification/tmp/filters"
+path_to_tmp = "./tmp"
+path_to_model = "./src/tumorclassification/modello_rf.pkl"
+path_to_filters = "./tmp/filters"
